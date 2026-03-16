c5c4e8dedaf6cffa4f76a5f5763c440d46657cb2
Alex Samuel <alexanderdsamuel@gmail.com>
2026-03-16 13:55:44 +0000

    First commit of battleship project.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Welcome to Battleships ! \n";
+    cout << "Game created by Alex Samuel";
+}
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
