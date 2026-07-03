758bb27c8723421a486e23f4f721f4d46839ac78
Alex Samuel <alexanderdsamuel@gmail.com>
2026-07-03 11:45:27 +0100

    changed method assignment inline with method name change

--- main.cpp
+++ main.cpp
@@ -409,10 +409,10 @@ void validateComputerShipAssignment(char gameGrid[GRID_SIZE][GRID_SIZE], char co
 
 void assignComputerShips (char gameGrid [GRID_SIZE][GRID_SIZE], int delayMS) {
     typewrite("Computer Assigning Ships ...", delayMS);
-    assignComputerShips(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 5, randomComputerShipOrientation(), 'C');
-    assignComputerShips(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 4, randomComputerShipOrientation(), 'B');
-    assignComputerShips(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 3, randomComputerShipOrientation(), 'R');
-    assignComputerShips(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 2, randomComputerShipOrientation(), 'D');
+    validateComputerShipAssignment(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 5, randomComputerShipOrientation(), 'C');
+    validateComputerShipAssignment(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 4, randomComputerShipOrientation(), 'B');
+    validateComputerShipAssignment(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 3, randomComputerShipOrientation(), 'R');
+    validateComputerShipAssignment(gameGrid, randomComputerShipColumn(), randomComputerShipRow(), 2, randomComputerShipOrientation(), 'D');
     displayGrid(gameGrid);
     typewrite("Computer Ships Assigned ...", delayMS);
     enterToContinue(delayMS);
