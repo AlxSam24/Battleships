87136c9c79b26c5efcf5f72f2f8f47df01cedcba
Alex Samuel <alexanderdsamuel@gmail.com>
2026-07-14 15:04:19 +0100

    Added relevant member variables and methods for player class

--- Player.h
+++ Player.h
@@ -3,7 +3,17 @@
 
 
 class Player {
+public:
+    virtual ~Player() = default;
+    virtual void placeShips() = 0;
+    virtual Coordinate chooseTarget(const Grid& opponentTracking) = 0;
+    virtual void notifyResult (Coordinate target, bool hit, Ship* sunkShip) {}
+    Grid& ownGrid();
+    Grid& trackingGrid();
+protected:
+    Grid ownGrid_;
+    Grid trackingGrid_;
+    Fleet fleet_;
 };
 
-
-#endif //BATTLESHIPS_PLAYER_H
+#endif
