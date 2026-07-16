2de99f694b5868908f6312c701c01f89c7e09a34
Alex Samuel <alexanderdsamuel@gmail.com>
2026-07-16 11:13:08 +0100

    Added Ship class and updated CMakeLists.txt

---
+++ Ship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alexa on 16/07/2026.
+//
+
+#include "Ship.h"
---
+++ Ship.h
@@ -0,0 +1,13 @@
+//
+// Created by alexa on 16/07/2026.
+//
+
+#ifndef BATTLESHIPS_SHIP_H
+#define BATTLESHIPS_SHIP_H
+
+
+class Ship {
+};
+
+
+#endif //BATTLESHIPS_SHIP_H
