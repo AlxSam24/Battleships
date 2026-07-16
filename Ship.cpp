b872c4a207bcb38019028852e840369a9d2081c0
Alex Samuel <alexanderdsamuel@gmail.com>
2026-07-16 11:35:53 +0100

    added one method and object creation of ships. NEEDS TO BE COMPLETED

--- Ship.cpp
+++ Ship.cpp
@@ -1,1 +1,12 @@
 #include "Ship.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Ship::Ship(string name, int length, char symbol)
+    :name_(name), length_(length), symbol_(symbol)
+{
+}
+void Ship :: registerHit() {
+    //Finish implementing methods
+}
